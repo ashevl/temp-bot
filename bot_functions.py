3c9f69284f3e433dfd82e5e2f67344da9ca88e8f
ashevl <ashevlane@gmail.com>
2021-11-11 20:07:22 +0000

    Added a module to pull the temperature and humidity data into a spreadsheet using openpyxl.

--- bot_functions.py
+++ bot_functions.py
@@ -30,3 +30,37 @@ def dht22_to_text():
 	f = open("current_temp.txt", "w")
 	f.write(text.format(current_time,temp,hum))
 	f.close()
+
+def dht22_to_xlsx():
+
+	import time
+	import datetime
+	import board
+	import adafruit_dht
+	from openpyxl import load_workbook
+	from datetime import date
+
+	today = date.today()
+	now = datetime.datetime.now().time()
+
+	dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+
+	temp = dhtDevice.temperature
+	hum = dhtDevice.humidity
+
+	#Repeats to make sure there is a good reading.
+	temp = dhtDevice.temperature
+	hum = dhtDevice.humidity
+
+	wb = load_workbook('temperature.xlsx')
+	sheet = wb['Sheet1']
+
+	row = (today, now, temp, hum)
+	sheet.append(row)
+
+	print(today)
+	print(now)
+	print(temp)
+	print(hum)
+
+	wb.save('temperature.xlsx')
---
+++ excel.py
@@ -0,0 +1,4 @@
+import twitter
+import bot_functions
+
+bot_functions.dht22_to_xlsx()
